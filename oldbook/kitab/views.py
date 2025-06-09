d485a1edba1cf068c5b3d3c8fb37a1ef08d649b8
shivamkumar93 <bcashivam11@gmail.com>
2025-06-09 23:48:24 +0530

    login page work is done

--- oldbook/kitab/views.py
+++ oldbook/kitab/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from kitab.models import *
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
-
+# pubic panel views
 def home(request):
-    return render(request, "public-panel/home.html")
+    data = {}
+    data['categories'] = Category.objects.all()
+    return render(request, "public-panel/home.html", data)
 
 def header(request):
     data = {}
@@ -14,3 +17,19 @@ def header(request):
 
 def footer(request):
     return render(request, 'public-panel/footer.html')
+
+# user panel views
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(home)
+        else:
+            (request, "invalid username or password")
+    return render(request, 'user-panel/login.html')
+#admin panel views
--- oldbook/oldbook/urls.py
+++ oldbook/oldbook/urls.py
@@ -4,9 +4,15 @@ from django.urls import path
 from kitab.views import *
 
 urlpatterns = [
+    #public panel urls
     path('admin/', admin.site.urls),
     path("", home, name="homepage"),
     path("header/",header, name="headerpage"),
     path("footer/",footer, name="footerpage"),
+
+    #user panel urls
+    path("login/", login_view, name="loginpage")
+
+    #admin panel urls
 
 ]
